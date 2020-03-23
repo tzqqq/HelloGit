789c1a80ca897a00f235a1f44da4243b8aae2b86
tzq <157116355@qq.com>
2020-03-23 21:55:27 +0800

    newdemo

--- node-demo/server.js
+++ node-demo/server.js
@@ -20,24 +20,30 @@ var server = http.createServer(function(request, response){
   /******** 从这里开始看，上面不要看 ************/
 
   console.log('有个傻子发请求过来啦！路径（带查询参数）为：' + pathWithQuery)
-  response.write('Hi')
+
+
+  if(path=='/style'){
+	  response.setHeader('Content-Type','text/css;charset=utf-8')
+	  response.write('stupid')
+	  response.end()
+	}else if (path=='/script'){
+		response.setHeader('Content-Type','text/javascript;charset=utf-8')
+		response.write('alert("这是jS执行的")')
+		response.end()  
+	  
+	}else if(path=='/index'){
+	  
+	  response.write('<!DOCTYPE html>\n<html>'+
+	  '<head><link rel="styleSheet" href="/style">'+
+	  '</head><body>'+
+	  '<b>hello</b>'+
+	  '<script src="/script"></script>'+ 
+	  '</body></html>')
+	  	response.end()  
+	}
+  
+  
   return response.end()
-  if(path === '/'){
-    response.statusCode = 200
-    response.setHeader('Content-Type', 'text/html;charset=utf-8')
-    response.write(`二哈`)
-    response.end()
-  } else if(path === '/x'){
-    response.statusCode = 200
-    response.setHeader('Content-Type', 'text/css;charset=utf-8')
-    response.write(`body{color: red;}`)
-    response.end()
-  } else {
-    response.statusCode = 404
-    response.setHeader('Content-Type', 'text/html;charset=utf-8')
-    response.write(`你输入的路径不存在对应的内容`)
-    response.end()
-  }
 
   /******** 代码结束，下面不要看 ************/
 })
